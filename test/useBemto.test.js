d7b30067da8d3a71d88acd2912934b7763e3ef61
Roman Komarov <kizmarh@ya.ru>
2019-05-27 14:28:41 +0200

    feat(react): Nested component test

--- test/useBemto.test.js
+++ test/useBemto.test.js
@@ -22,16 +22,16 @@ const Foo = props => {
 
 test("Basic react component", () => {
   expect(reactElement(Foo, { className: "Foo" })).toMatchInlineSnapshot(`
-                        <span
-                          className="Foo"
-                        >
-                          <em
-                            className="Foo__Content"
-                          >
-                            Hello, world!
-                          </em>
-                        </span>
-            `);
+                            <span
+                              className="Foo"
+                            >
+                              <em
+                                className="Foo__Content"
+                              >
+                                Hello, world!
+                              </em>
+                            </span>
+              `);
 });
 
 test("Basic react component with some passed props and modifiers", () => {
@@ -45,75 +45,91 @@ test("Basic react component with some passed props and modifiers", () => {
       }
     })
   ).toMatchInlineSnapshot(`
-                <span
-                  className="Foo Foo_foo"
-                >
-                  <em
-                    className="Foo__Content"
-                    title="Content title"
-                  >
-                    Hello, world!
-                  </em>
-                </span>
-        `);
+                    <span
+                      className="Foo Foo_foo"
+                    >
+                      <em
+                        className="Foo__Content"
+                        title="Content title"
+                      >
+                        Hello, world!
+                      </em>
+                    </span>
+          `);
 });
 
-
 test("Dynamic props change", () => {
-  const initialComponent = React.createElement(
-    Foo,
-    {
-      className: "Foo"
-    }
-  );
+  const initialComponent = React.createElement(Foo, {
+    className: "Foo"
+  });
 
   const fooRenderer = reactRenderer.create(initialComponent);
   expect(fooRenderer.toJSON()).toMatchInlineSnapshot(`
+            <span
+              className="Foo"
+            >
+              <em
+                className="Foo__Content"
+              >
+                Hello, world!
+              </em>
+            </span>
+      `);
+
+  const updatedComponent1 = React.createElement(Foo, {
+    className: "Foo1",
+    __Content: { title: "foo" }
+  });
+
+  fooRenderer.update(updatedComponent1);
+  expect(fooRenderer.toJSON()).toMatchInlineSnapshot(`
         <span
-          className="Foo"
+          className="Foo1"
         >
           <em
-            className="Foo__Content"
+            className="Foo1__Content"
+            title="foo"
           >
             Hello, world!
           </em>
         </span>
     `);
 
-  const updatedComponent1 = React.createElement(
-    Foo,
-    {
-      className: "Foo1",
-      __Content: { title: "foo" }
-    }
-  );
-
-  fooRenderer.update(updatedComponent1);
-  expect(fooRenderer.toJSON()).toMatchInlineSnapshot(`
-    <span
-      className="Foo1"
-    >
-      <em
-        className="Foo1__Content"
-        title="foo"
-      >
-        Hello, world!
-      </em>
-    </span>
-  `);
-
-  const updatedComponent2 = React.createElement(
-    Foo,
-    {
-      className: "Foo2",
-      __Content: null,
-    }
-  );
+  const updatedComponent2 = React.createElement(Foo, {
+    className: "Foo2",
+    __Content: null
+  });
 
   fooRenderer.update(updatedComponent2);
   expect(fooRenderer.toJSON()).toMatchInlineSnapshot(`
+        <span
+          className="Foo2"
+        />
+    `);
+});
+
+const Bar = props => {
+  const { Root, Content } = useBemto(props, {
+    __Root: "span",
+    __Content: Foo
+  });
+  return React.createElement(Root, {}, React.createElement(Content, {}));
+};
+
+test("Component as an element", () => {
+  expect(reactElement(Bar, { className: "Bar" })).toMatchInlineSnapshot(`
     <span
-      className="Foo2"
-    />
+      className="Bar"
+    >
+      <span
+        className="Bar__Content"
+      >
+        <em
+          className="Bar__Content__Content"
+        >
+          Hello, world!
+        </em>
+      </span>
+    </span>
   `);
 });
