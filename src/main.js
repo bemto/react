4ccd8b2e58450a9bd672ab79fa7755a9e0a453f9
Roman Komarov <kizmarh@ya.ru>
2019-06-03 16:58:14 +0200

    feat(chore): Export core bemto

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import useBemto from './useBemto.js';
+import { bemto, withSettings } from '@bemto/core';
 import { getButtonStyles, ButtonComponent } from './BemtoButton.js';
 import { getInputStyles, InputComponent } from './BemtoInput.js';
+import useBemto from './useBemto.js';
 
 export {
-  useBemto,
+  bemto, withSettings,
   getButtonStyles, ButtonComponent,
   getInputStyles, InputComponent,
+  useBemto,
 };
